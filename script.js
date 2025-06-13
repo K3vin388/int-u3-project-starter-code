8cd42ef2d7f842a169f967adc8583221a666e81a
K3vin388 <cruzverakevin18@gmail.com>
2025-06-13 08:57:35 -0400

    Updated Code

--- script.js
+++ script.js
@@ -1,26 +1,110 @@
 // Declare variables below to save the different divs of your story.
-
-
-
-
-
-
-
-
-
+let optionOne = document.querySelector (".option-one");
+let optionTwo = document.querySelector(".option-two");
+let optionOneScreen = document.querySelector(".option-one-screen");
+let optionTwoScreen = document.querySelector(".option-two-screen");
+let title = document.querySelector(".title");
+let storyOpening = document.querySelector('.story-opening');
+let optionOneEnd = document.querySelector(".option-one-end");
+let optionTwoEnd = document.querySelector(".option-two-end");
+let next = document.querySelector(".option-three");
+let nature = document.querySelector(".nature")
+let clock = document.querySelector(".clock")
+let ocean = document.querySelector(".ocean-three")
+let congrats = document.querySelector(".option-two-end")
 // When you're ready to make event handlers, uncomment the code below. 
 //  - Then fill in the blanks with the correct variables.
 
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+ optionOne.addEventListener('click', function(){
+    optionOneScreen.style.display = "block";
+    storyOpening.style.display  = "none";
+    title.style.display = "none"; 
+    optionTwo.style.display = "none";
+    optionTwoScreen.style.display = "none";
 
-// });
-
-// INSERT_VARIABLE.addEventListener('click', function(){
-
-// });
 
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+ });
 
-// });
+    nature.addEventListener('click', function(){
+    optionOneEnd.style.display = "block";
+    title.style.display = "none";
+    storyOpening.style.display = "none";
+    optionOne.style.display = "none";
+    optionOneScreen.style.display = "none";
+    optionTwo.style.display = "none";
+    optionTwoScreen.style.display = "none";
+
+ocean.addEventListener("click" , function(){ 
+
+  
+});
+
+
+ });
+ title.addEventListener("click" , function(){     
+    title.style.display = "none";
+    storyOpening.style.display = "none";
+    optionOne.style.display = "block";
+    optionOneScreen.style.display = "none";
+    optionTwo.style.display = "block";
+    optionTwoScreen.style.display = "none";
+
+ });
+ clock.addEventListener("click" , function(){ 
+    storyOpening.style.display = "block" 
+    title.style.display = "none";
+    optionOneEnd.style.display ="none"
+    optionOne.style.display = "block";
+    optionTwo.style.display = "block"
+    optionOneScreen.style.display = "none";
+
+ });
+
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ optionTwo.addEventListener('click', function(){
+ optionTwoScreen.style.display = "block";
+ storyOpening.style.display = "none";
+ title.style.display = "none";
+
+ });
+ ocean.addEventListener("click" , function(){
+    optionTwoEnd.style.display = "block";
+    storyOpening.style.display = "none"
+    optionTwoScreen.style.display = "none"
+    optionOne.style.display = "none";
+    optionOneScreen.style.display = "none";
+    optionOneEnd = "none";
+ });
+ 
+
+ 
+ optionTwoEnd.addEventListener("click", function(){
+   optionOneEnd.style.display = "block";
+
+ });
+
